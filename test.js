c9ee973757f82113a966025ed2e20fa3e66e1c75
jojogos-home <xiniliwy@163.com>
2017-06-21 22:37:24 +0800

    -1706212237

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 
-let fns=[Function,Object,Number,Boolean,String,Array,Date];
-let ar=TDATA.num_ar;
+let fns = [Function, Object, Number, Boolean, String, Array, Date];
+let ar = TDATA.num_ar;
+let log = console.log;
 //////////////////////////////////////////////////
 
 
 println("iLibs.es.doneClick([function]");
 println("iLibs.es.actionClick([function]");
-println("Libs",Object.getOwnPropertyNames(Libs));
-println("iLibs",Object.getOwnPropertyNames(iLibs));
+println("Libs", Object.getOwnPropertyNames(Libs));
+println("iLibs", Object.getOwnPropertyNames(iLibs));
 ///////////////////////////////////////////////////////
 
-
-
-        
+$.getJSON('td.json',function(jsonObj){
+    $.each(jsonObj,function(idx,obj){
+        println(idx,obj.Airplane);
+    });
+})
