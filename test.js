db354f119cc49f94be3b68c67954e3f70aefb3eb
jojogos-home <xiniliwy@163.com>
2017-06-24 21:08:09 +0800

    -170624

--- test.js
+++ test.js
@@ -1,23 +1,31 @@
-let fns = [Function, Object, Number, Boolean, String, Array, Date];
-let ar = TDATA.num_ar;
+
 let log = console.log;
 
 //////////////////////////////////////////////////
 function searchPhone(_val) {
-    let finded = findInComunications((v) => _val == v.name);
+    let finded = findInComunications((v) => _val == v.name),
+        viewer = $("#shower").children("span:eq(0)");
     if (finded) {
-        $('#header-out1').text(finded.phone);
+        viewer.html(`${finded.name}:${finded.phone}`);
     } else {
-        $('#header-out1').text("Not Find " + _val);
+        viewer.html(`Not Find '${_val}'.`);
     }
     return true;
 }
-
+function getVdtString(){
+    return new Date().toLocaleString().slice(0,-3);
+}
 
 $('document').ready(function () {
     window.moveTo(1, 1);
+    let ho1 = $("#header-out1");
+
+    ho1.html(getVdtString());
+    setInterval(()=>ho1.html(getVdtString()),1000*60);
+
     es.relinPressed(searchPhone);
     es.doneClick(searchPhone);
+
     let dbs = store.get('comunications'),
         namess = $("#namess");
     dbs.forEach(v => namess.append($("<option>").val(v.name)));
